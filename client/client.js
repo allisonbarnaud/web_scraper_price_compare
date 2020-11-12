b4f1de228d087adaa76022ad65b43ca39ce299a5
Jimmy <jimmypannu_18@yahoo.co.in>
2020-11-12 18:41:40 +1100

    first commit

---
+++ client/client.js
@@ -0,0 +1,7 @@
+const userForm = document.querySelector('.user-form');
+const userInput = document.querySelector('.userinput');
+const submitBtn = document.querySelector('.submit-btn');
+const laptops = document.querySelector('.laptops');
+const smartphones = document.querySelector('.smartphones');
+const monitors = document.querySelector('.monitors');
+const printers = document.querySelector('.printers');
